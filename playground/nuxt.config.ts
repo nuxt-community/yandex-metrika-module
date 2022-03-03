2f1d41593a34fabdac01095dd875351a21264a57
Dan <78473524+dankerow@users.noreply.github.com>
2022-03-03 09:09:06 -0800

    fix: Updated playground/nuxt.config.ts
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 import YandexMetrikaModule from '..'
 
 export default defineNuxtConfig({
-  dev: true,
-  render: {
-    resourceHints: false
-  },
   modules: [
     YandexMetrikaModule
   ],
